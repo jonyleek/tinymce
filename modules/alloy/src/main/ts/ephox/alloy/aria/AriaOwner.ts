bb495386340bd27287bf739b74653edd5c462eaa
Lee Newson <lee.newson@tiny.cloud>
2022-02-03 16:00:36 +1000

    TINY-8417: Further cleanup to replace all uses of aria-owns with aria-controls

--- modules/alloy/src/main/ts/ephox/alloy/aria/AriaOwner.ts
+++
@@ -1,9 +0,0 @@
-import * as AriaManager from './AriaManager';
-
-const find = AriaManager.find(AriaManager.LinkableAttribute.AriaOwns);
-const manager = AriaManager.build(AriaManager.LinkableAttribute.AriaOwns);
-
-export {
-  find,
-  manager
-};
